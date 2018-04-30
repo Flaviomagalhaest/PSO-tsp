ba17cf9d578fb8952d7be781b99dfee016a2a613
Flavio <flaviomagalhaest@gmail.com>
2018-04-30 15:40:27 -0300

    [PSO-tsp] Correção e criação de mais tests

--- tests.py
+++ tests.py
@@ -1,15 +1,27 @@
 from django.test import TestCase
 from py.tsp import PontosClass as P
 from py.tsp import tspController as tspController
-import os
+from py.tsp import IndividuoClass as I
+import os, json
 
 
 class PSOTspTestCase(TestCase):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psoweb.settings")
 
+    #Criando pontos para testes
+    pontosJsonString = '[{"x": 4, "y": 0}, {"x": 2, "y": 2}, {"x": 1, "y": 1}]'
     pontosJson = [{'x': 4, 'y': 0}, {'x': 2, 'y': 2}, {'x': 1, 'y': 1}]
     pontosObj = tspController.calcMatrixDist(pontosJson)    
 
+    #Criando individuos para testes
+    indivJson = []
+    indivJson.append('{"atual": [5, 4, 0, 3, 9, 6, 7, 8, 2, 1, 5], "pbest": [], "distAtual": 4415.696909091138, "distPbest": 0, "gbest": false}')
+    indivJson.append('{"atual": [1, 4, 9, 2, 5, 3, 8, 7, 6, 0, 1], "pbest": [], "distAtual": 4006.7252376388124, "distPbest": 0, "gbest": false}')
+    indivJson.append('{"atual": [6, 3, 7, 4, 8, 1, 2, 0, 5, 9, 6], "pbest": [], "distAtual": 4472.919868660942, "distPbest": 0, "gbest": false}')
+    individuos = []
+    for indiv in indivJson:
+        individuos.append(I.Individuo(json=json.loads(indiv)))    
+    
     def test_geraMatrizDistanciaCorretamente(self):    
         self.assertEqual(self.pontosObj.pontos[0].matrixDist,[0.0, 2.8284271247461903, 3.1622776601683795])
         self.assertEqual(self.pontosObj.pontos[1].matrixDist,[2.8284271247461903, 0.0, 1.4142135623730951])
@@ -34,6 +46,16 @@ class PSOTspTestCase(TestCase):
         self.assertEqual(pontos.pontos[2].x, 1)
 
     def test_geraCorretamentePopInicial(self):
-        individuos = tspController.geraPopInicial(2, 3, self.pontosObj)
+        individuos = tspController.geraPopInicial(2, 3, self.pontosObj.toJson())
         self.assertEqual(len(individuos), 2)
 
+    def test_defineGbest(self):
+        gbest = tspController.atualizaGbest(self.individuos)
+        self.assertEqual(gbest.toJson(), '{"atual": [1, 4, 9, 2, 5, 3, 8, 7, 6, 0, 1], "pbest": [], "distAtual": 4006.7252376388124, "distPbest": 0, "gbest": true}')
+        self.assertEqual(self.individuos[1].gbest, True)
+    
+    def test_definePbests(self):
+        individuos = tspController.atualizaPbest(self.individuos)
+        self.assertEqual(individuos[0].atual, individuos[0].pbest)
+        self.assertEqual(individuos[1].atual, individuos[1].pbest)
+        self.assertEqual(individuos[2].atual, individuos[2].pbest)
